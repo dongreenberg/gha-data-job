32665238bd78487d698f9266a9867ef44ffa19fe
dongreenberg <dongreenberg2@gmail.com>
2024-05-29 17:28:40 -0400

    Fix s3

--- batch_process.py
+++ batch_process.py
@@ -9,14 +9,14 @@ from embedder import URLEmbedder
 
 def generate_url_embeddings(base_url: str):
     # We recursively extract all children URLs from the given base URL.
-    urls = extract_urls(base_url, max_depth=1)
+    urls = extract_urls(base_url, max_depth=1)[:4]
     url_df = pd.DataFrame(urls, columns=["url"])
 
     # Set up an embedder to embed the URLs on the fly on a remote GPU box
-    cluster = rh.cluster(f"rh-hamilton-a10g",
+    cluster = rh.cluster(f"/dongreenberg/rh-hamilton-a10g",
                          instance_type="A10G:1",
                          auto_stop_mins=5,
-                         spot=True).up_if_not()
+                         spot=True).up_if_not().save()
     env = rh.env(
         name=f"langchain_embed_env",
         reqs=["langchain", "langchain-community", "langchainhub", "sentence_transformers", "bs4"],
@@ -32,12 +32,13 @@ def generate_url_embeddings(base_url: str):
     url_df["embeddings"] = [embedder.embed(url, normalize_embeddings=True) for url in urls]
 
     # Save the embeddings to disk
-    url_df.to_csv("url_embeddings.csv", index=False)
+    branch = os.environ.get("GITHUB_HEAD_REF", "main")
+    os.makedirs(branch, exist_ok=True)
+    url_df.to_csv(f"{branch}/url_embeddings.csv", index=False)
 
     # Send to S3
     # Get the current git revision to track the code version
-    branch = os.environ.get("GITHUB_HEAD_REF", "main")
-    rh.folder(path="url_embeddings.csv").to(f"s3://url_embeddings/url_embeddings.csv@{branch}")
+    rh.folder(path=f"{branch}").to(system="s3", path=f"/url_embeddings/{branch}")
 
 
 if __name__ == "__main__":
